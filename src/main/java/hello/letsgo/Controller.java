2dafec4ffab925e88f780ca595b89b33cd5b9a8c
geung <geungan9@gmail.com>
2023-05-27 09:21:32 +0900

    test

---
+++ src/main/java/hello/letsgo/Controller.java
@@ -0,0 +1,13 @@
+package hello.letsgo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api")
+    public String api() {
+        return "first";
+    }
+}
